7da6a22b3ad34acf8b83e68c681d09fec6a4a758
B. Endres <endres@systopia.de>
2023-10-17 11:15:30 +0200

    improved log messages

--- CRM/Committees/Implementation/SessionSyncer.php
+++ CRM/Committees/Implementation/SessionSyncer.php
@@ -228,7 +228,7 @@ class CRM_Committees_Implementation_SessionSyncer extends CRM_Committees_Plugin_
         }
         if ($changed_emails) {
             $changed_emails_count = count($changed_emails);
-            $this->log("Some attributes have changed for {$changed_emails_count} emails, be we won't adjust that.");
+            $this->log("Some attributes have changed for {$changed_emails_count} emails, but won't adjust that.");
         }
         if ($obsolete_emails) {
             $obsolete_emails_count = count($obsolete_emails);
@@ -259,7 +259,7 @@ class CRM_Committees_Implementation_SessionSyncer extends CRM_Committees_Plugin_
         }
         if ($changed_phones) {
             $changed_phones_count = count($changed_phones);
-            $this->log("Some attributes have changed for {$changed_phones_count}, be we won't adjust that.");
+            $this->log("Some attributes have changed for {$changed_phones_count} phones, but won't adjust that.");
         }
         if ($obsolete_phones) {
             $obsolete_phones_count = count($obsolete_phones);
@@ -294,7 +294,7 @@ class CRM_Committees_Implementation_SessionSyncer extends CRM_Committees_Plugin_
         }
         if ($changed_addresses) {
             $changed_addresses_count = count($changed_addresses);
-            $this->log("Some attributes have changed for {$changed_addresses_count}, be we won't adjust that.");
+            $this->log("Some attributes have changed for {$changed_addresses_count} addresses, but won't adjust that.");
         }
         if ($obsolete_addresses) {
             $obsolete_addresses_count = count($obsolete_addresses);
